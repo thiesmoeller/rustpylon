e7f7d6b62721e6e2d6e4ad1d459ee17bd06a4e90
Falco Hirschenberger <falco.hirschenberger@itwm.fraunhofer.de>
2020-07-13 10:41:02 +0200

    Add windows support

--- pylon-sys/build.rs
+++ pylon-sys/build.rs
@@ -2,15 +2,26 @@ use bindgen;
 use std::env;
 use std::path::PathBuf;
 use std::fs;
+use std::path::Path;
 
 fn main() {
-    let pylon_root = env!("PYLON_ROOT");
-    let pylon_include = format!("{}/include", pylon_root);
-    let pylon_libs = format!("{}/lib64", pylon_root);
+    let pylon_root = Path::new(env!("PYLON_ROOT"));
+    let pylon_include = pylon_root.join("include");
+
+    let pylon_libs = if env::var("CARGO_CFG_WINDOWS").is_ok() {
+            pylon_root.join("lib").join("x64")
+        } else if env::var("CARGO_CFG_UNIX").is_ok() {
+            pylon_root.join("/lib64")
+        } else {
+            panic!("Unexpected target platform");
+        };
+
+
+
 
     println!("cargo:rustc-link-lib=pylonc");
-    println!("cargo:rustc-link-lib=pylonbase");
-    println!("cargo:rustc-link-lib=pylonutility");
+    println!("cargo:rustc-link-lib=pylonbase_v6_1");
+    println!("cargo:rustc-link-lib=pylonutility_v6_1");
 
     // find all additional libs in the pylon libs dir. They may have different names, so glob them
     let additional = &["libGenApi_gcc", "libGCBase_gcc", "libLog_gcc", "libMathParser_gcc", "libXmlParser_gcc", "libNodeMapData_gcc" ];
@@ -19,13 +30,13 @@ fn main() {
             if let Ok(p) = path {
                 let file = p.file_name().into_string().unwrap();
                 if additional.iter().any(|lib| file.starts_with(lib)) {
-                    println!("cargo:rustc-link-lib={}", file.trim_start_matches("lib").trim_end_matches(".so"));
+                    println!("cargo:rustc-link-lib={}", file.trim_start_matches("lib").trim_end_matches(".dll"));
                 }
             }
         }
     };
 
-    println!("cargo:rustc-link-search={}", pylon_libs);
+    println!("cargo:rustc-link-search={}", pylon_libs.to_str().unwrap());
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -34,10 +45,11 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
-        .clang_arg(format!("-I{}", pylon_include))
-        .clang_arg(format!("-L{}", pylon_libs))
-        .default_enum_style(bindgen::EnumVariation::Rust{non_exhaustive: false})
-        .derive_default(true)
+        .clang_arg(format!("-I{}", pylon_include.to_str().unwrap()))
+        .clang_arg(format!("-L{}", pylon_libs.to_str().unwrap()))
+        .default_enum_style(bindgen::EnumVariation::Rust)
+        .derive_default(false)
+        .derive_debug(false)
         .opaque_type("PYLON_DEVICE_HANDLE")
         .opaque_type("PYLON_STREAMGRABBER_HANDLE")
         .opaque_type("PYLON_STREAMBUFFER_HANDLE")
